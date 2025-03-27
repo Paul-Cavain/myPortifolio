da68dc6e4a573a2e214db659f0c72c0439dd43bd
Paul-Cavain <paulcavain01@gmail.com>
2025-03-27 09:09:45 +0300

    testmonial

--- src/pages/PortifolioPages/Testimonies.js
+++ src/pages/PortifolioPages/Testimonies.js
@@ -1,22 +1,25 @@
+import ArrowScroll from "./ArrowScroll";
 import Carousels from "./Carousels";
-import Footer from "../../components/Common/Footer";
-import Navbar from "../../components/Common/Navbar";
 
-const Testimonies = () => {
+const Testimonies = ({ testimonials }) => {
   return (
-    <section>
-      <nav>
-        <Navbar />
-      </nav>
+    <div className="pt-32 pb-20 md:pb-0 px-4 md:px-28">
+      <h3 className="text-xl text-center">Testimonies</h3>
+      <p className="pt-10 md:px-72 md:text-balance text-center">
+        Hi, I am Paulo Nkelego, a software engineer passionate about building
+        efficient and scalable software systems. Here, I share insights, tips,
+        and experiences from my journey in tech. Thanks for stopping by —let us
+        explore, learn, and grow together!
+      </p>
 
-      <main className="mt-24">
-        <Carousels />
-      </main>
+      {/* carousel testimonials */}
+      <Carousels testimonials={testimonials} />
 
-      <footer>
-        <Footer />
-      </footer>
-    </section>
+      {/* Arrow srcoll */}
+      <div className="flex md:hidden">
+        <ArrowScroll />
+      </div>
+    </div>
   );
 };
 
--- src/pages/PortifolioPages/Works.js
+++ src/pages/PortifolioPages/Works.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ArrowScroll from "./ArrowScroll";
-import { BsGithub } from "react-icons/bs";
+import { BsEye, BsGithub } from "react-icons/bs";
 
 const Works = ({ projects }) => {
   return (
@@ -12,7 +12,7 @@ const Works = ({ projects }) => {
         {projects.map((project) => (
           <div
             key={project.id}
-            className="border-2 border-black rounded-sm shadow-md w-full min-h-56"
+            className="border-2 border-black rounded-md shadow-md w-full min-h-56"
           >
             <img
               src={project.image}
@@ -22,14 +22,12 @@ const Works = ({ projects }) => {
             <div className="p-4">
               <p className="text-lg font-semibold flex flex-row justify-between items-center">
                 {project.title}
-                <Link to={project.projectLink}>
-                  <span className="text-xl px-4 py-1 text-black bg-white w-fit transition-all ease-out duration-500 cursor-pointer font-normal border-2 border-black rounded-sm hover:bg-black hover:text-white text-center">
-                    visit
-                  </span>
+                <Link to={project.projectLink} target="blank">
+                  <BsEye className="size-8 bg-white text-black ring-1 ring-black hover:bg-black hover:text-white hover:ring-1 hover:ring-black rounded-md p-2 hover:p-2" />
                 </Link>
               </p>
-              <div className="flex flex-row gap-x-4 justify-between items-center mt-10">
-                <ul className="flex flex-row gap-x-2 md:gap-x-4 justify-start items-start">
+              <div className="flex flex-row justify-between items-center mt-10">
+                <ul className="flex flex-row gap-x-2 md:gap-x-1 justify-start items-start">
                   <li className="bg-gray-400 w-fit px-1.5 md:px-2 py-1 rounded-sm">
                     {project.technologies[0]}
                   </li>
@@ -39,10 +37,15 @@ const Works = ({ projects }) => {
                   <li className="bg-gray-400 w-fit px-1.5 md:px-2 py-1 rounded-sm">
                     {project.technologies[2]}
                   </li>
+                  {project.technologies[3] && (
+                    <li className="bg-gray-400 w-fit px-1.5 md:px-2 py-1 rounded-sm">
+                      {project.technologies[3]}
+                    </li>
+                  )}
                 </ul>
                 <p>
-                  <Link to={project.githubLink}>
-                    <BsGithub className="size-6" />
+                  <Link to={project.githubLink} target="blank">
+                    <BsGithub className="size-8 bg-black text-white hover:bg-white hover:text-black hover:ring-1 hover:ring-black rounded-md p-2 hover:p-2" />
                   </Link>
                 </p>
               </div>
